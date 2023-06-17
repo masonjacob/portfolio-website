6ad2bbc4a57db5e661e76b454199fd79bad1506a
Mason Jacob <masonjacob@uri.edu>
2023-06-16 22:27:53 -0400

    Social Icon Widget V1

--- src/app/social-icons/page.tsx
+++ src/app/social-icons/page.tsx
@@ -1,63 +1,28 @@
-'use client'
-import Image from 'next/image'
-import { useState } from 'react';
-import { motion } from 'framer-motion';
+'use client';
+import { useEffect, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import SocialIcon from '../../components/SocialIcon';
 
 const socialIcons = [
-  { name: 'Facebook', icon: '/Facebook_icon.svg', info: 'Facebook Widget' },
-  { name: 'GitHub', icon: '/GitHub_icon.svg', info: 'GitHub Widget' },
-  { name: 'Instagram', icon: '/Instagram_icon.svg', info: 'Instagram Widget' },
+  { name: 'Facebook', icon: '/facebook-icon.svg', info: 'Facebook Widget', direction: 'right', size: 1 },
+  { name: 'GitHub', icon: '/github-icon.svg', info: 'GitHub Widget', direction: 'right', size: 1 },
+  { name: 'LinkedIn', icon: '/linkedin-icon.svg', info: 'LinkedIn Widget', direction: 'right', size: 1 },
   // Add more social icons as needed
 ];
 
 const SocialIconsPage: React.FC = () => {
-  const [isAnimationComplete, setIsAnimationComplete] = useState(false);
-
-  const handleAnimationComplete = () => {
-    setIsAnimationComplete(true);
-    
-  };
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      {!isAnimationComplete && (
+    <>
+      {socialIcons.map((socialIcon, index) => (
         <motion.div
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          onAnimationComplete={handleAnimationComplete}
+          key={socialIcon.name}
+          whileHover={{ scale: 1.1 }}
+          className="col-span-3 flex justify-center items-center"
         >
-            <Image 
-              src="/vector-portrait.svg"
-              alt="Headshot" 
-              className="w-20 h-20 rounded-full" 
-              fill
-              />
+          <SocialIcon key={socialIcon.name} icon={socialIcon.icon} name={socialIcon.name} info={socialIcon.info} direction={socialIcon.direction} />
         </motion.div>
-      )}
-
-      <div className="grid grid-cols-3 gap-4 mt-8">
-        {socialIcons.map((socialIcon) => (
-          <motion.div
-            key={socialIcon.name}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className="bg-gray-800 bg-opacity-50 rounded-lg overflow-hidden"
-          >
-            <Image 
-              src={socialIcon.icon}
-              alt={socialIcon.name} 
-              className="w-16 h-16"
-              fill
-            />
-            <div className="p-2">
-              <p className="text-white font-bold">{socialIcon.name}</p>
-              <p className="text-gray-300">{socialIcon.info}</p>
-            </div>
-          </motion.div>
-        ))}
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+interface SocialIconProps {
+  icon: string;
+  name: string;
+  info: string;
+  direction: 'right' | 'left';
+}
+
+const SocialIcon: React.FC<SocialIconProps> = ({ icon, name, info, direction }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded((prevExpanded) => !prevExpanded);
+  };
+
+  return (
+    <motion.div
+      className={`flex items-center bg-gray-800 bg-opacity-50 rounded-lg overflow-hidden ${
+        isExpanded ? (direction === 'right' ? 'expand-right' : 'expand-left') : ''
+      }`}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      onClick={toggleExpand}
+    >
+      <Image 
+        src={icon} 
+        alt={name} 
+        width={0}
+        height={0}
+        className="w-16 h-16" 
+      />
+
+      {isExpanded && (
+        <div className={`p-4 ${direction === 'right' ? 'ml-2' : 'mr-2'}`}>
+          <p className="text-white font-bold">{name}</p>
+          <p className="text-gray-300">{info}</p>
+        </div>
+      )}
+    </motion.div>
+  );
+};
+
+export default SocialIcon;
